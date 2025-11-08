e7935156663f35fc1fd72eca252db1f2c4678e8c
jesusenriquecabezaacero <jesusenriquecabezaacero@gmail.com>
2025-11-08 11:46:13 -0500

    Update in Firebase configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,13 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "friendlychat-edbee", appId: "1:701863645585:web:83e0626aaae14f1ff48a30", storageBucket: "friendlychat-edbee.firebasestorage.app", apiKey: "AIzaSyDZSMuQ4HZHwDU97vHY0Sd1WIzcxBVaG34", authDomain: "friendlychat-edbee.firebaseapp.com", messagingSenderId: "701863645585", projectNumber: "701863645585", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(
+      environment.firebase
+    )),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage())
   ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    projectId: "friendlychat-edbee",
+    appId: "1:701863645585:web:83e0626aaae14f1ff48a30",
+    storageBucket: "friendlychat-edbee.firebasestorage.app",
+    apiKey: "AIzaSyDZSMuQ4HZHwDU97vHY0Sd1WIzcxBVaG34",
+    authDomain: "friendlychat-edbee.firebaseapp.com",
+    messagingSenderId: "701863645585",
+    projectNumber: "701863645585",
+    version: "2"
   },
 };
