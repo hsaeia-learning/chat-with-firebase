eda09e2d8b1057b686f9d4f3a697112b1e2fb92b
jesusenriquecabezaacero <jesusenriquecabezaacero@gmail.com>
2025-11-08 11:02:51 -0500

    Install AngularFire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,6 +19,6 @@ export const appConfig: ApplicationConfig = {
       provideStorage(() => getStorage()),
       provideMessaging(() => getMessaging())
     ),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "friendlychat-edbee", appId: "1:701863645585:web:83e0626aaae14f1ff48a30", storageBucket: "friendlychat-edbee.firebasestorage.app", apiKey: "AIzaSyDZSMuQ4HZHwDU97vHY0Sd1WIzcxBVaG34", authDomain: "friendlychat-edbee.firebaseapp.com", messagingSenderId: "701863645585", projectNumber: "701863645585", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
